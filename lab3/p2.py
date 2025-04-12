81b4fa57314673ba2a8989ed813e3cbdd1bedea6
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-04-11 22:33:54 -0500

    esquema de p2 y la funcion insert de p1

--- lab3/p2.py
+++ lab3/p2.py
@@ -1,1 +1,45 @@
 # Integrantes: Mikel Bracamonte, Gino Daza
+import struct
+
+class Bucket:
+    size = 0
+
+    def __init__(self, fb):
+        self.next = -1
+        self.fb = fb
+
+class Hash:
+    D = 3
+    fb = 3
+    hash_index = list()
+
+    FORMAT = "iiii"
+    BUCKET_SIZE = struct.calcsize(FORMAT)
+
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.hash_index[0] = 0
+        self.hash_index[1] = 1
+
+    def hash(self, num: int) -> int:
+        return num % 2**self.D
+    
+    def Binary(self, num: int) -> str:
+        return bin(num)
+    
+    def insert(self, key: int):
+        binary = self.Binary(self.hash(key))
+        bucket = self.hash_index[binary]
+
+        with open(self.filename, "r+b") as file:
+            file.seek(self.BUCKET_SIZE * bucket)
+            data = struct.unpack(self.FORMAT, file.read)
+
+
+
+
+
+
+    def search(self, key: int):
+        pass
+    
