aba8495e83c79a8fc73df23516ac99d8236cb7ac
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-04-13 05:28:27 -0500

    ya esta casi toda la p2, solo falta guardar el hash_index en el archivo de alguna manera

--- lab3/p2.py
+++ lab3/p2.py
@@ -1,6 +1,7 @@
 # Integrantes: Mikel Bracamonte, Gino Daza
 import struct
 from typing import TextIO
+import os
 
 class Alumno:
     FORMAT = "i20s20s"
@@ -17,18 +18,16 @@ class Alumno:
     @staticmethod
     def unpack(packed_data: bytes) -> "Alumno":
         id, nombre, apellido = struct.unpack(Alumno.FORMAT, packed_data)
-        return Alumno(id, nombre.decode(), apellido.decode())
+        return Alumno(id, nombre.decode().strip(), apellido.decode().strip())
 
 class Bucket:
-    size = 0
-    next = -1
-    records: list[Alumno] = []
+    HEADER_FORMAT = "ii"
+    HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
-    FORMAT = "ii"
-    SIZE = struct.calcsize(FORMAT)
-
-    def __init__(self, fb):
+    def __init__(self, fb: int):
+        self.size = 0
         self.next = -1
+        self.records: list[Alumno] = []
         self.fb = fb
 
         for i in self.fb:
@@ -36,135 +35,216 @@ class Bucket:
 
     def pack(self) -> bytes:
         data: bytes
-        for alumno in self.records:
-            data += alumno.pack()
-        return data + struct.pack(self.FORMAT, self.size, self.next)
+        for record in self.records:
+            data += record.pack()
+        return data + struct.pack(self.HEADER_FORMAT, self.size, self.next)
+    
+    @staticmethod
+    def unpack(packed_data: bytes, fb: int) -> "Bucket":
+        bucket = Bucket(fb)
+        size, next = struct.unpack(Bucket.HEADER_FORMAT, packed_data[-8:])
+        for i in range(size):
+            bucket.records[i] = Alumno.unpack(packed_data[i * Alumno.SIZE: (i + 1) * Alumno.SIZE])
+        bucket.next = next
+
+        return bucket
     
     def is_full(self) -> bool:
         return self.size == self.fb
     
-    def add_alumno(self, alumno: Alumno):
-        pass
+    def add_record(self, record: Alumno):
+        if not self.is_full():
+            self.records.append(record)
+            self.size += 1
 
 class ExtendibleHash:
-    D = 3
-    CURRENT_DEPTH = 1
-    MAX_BUCKETS = 2**D
-    BUCKETS = 0
-    OVERFLOW_BUCKETS = 0
-    fb = 3
-    hash_index = list()
-
-    FORMAT = "i" * (fb + 2)
-    BUCKET_SIZE = struct.calcsize(FORMAT)
-
-    HEADER_FORMAT = "i"
+    HEADER_FORMAT = "iiiii"
     HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 
-    def __init__(self, filename: str): # TODO si el archivo ya existe, leer el header, si no existe, inicializar todo
+    def __init__(self, filename: str, D: int, fb: int):
         self.filename = filename
-        self.hash_index[0] = 0
-        self.hash_index[1] = 1
-        self.BUCKETS = 2
+
+        if os.path.exists(filename): # Leer toda la información
+            with open(filename, "rb") as file:
+                self.D, self.CURRENT_DEPTH, self.BUCKETS, self.OVERFLOW_BUCKETS, self.fb = struct.unpack(self.HEADER_FORMAT, file.read(self.HEADER_SIZE))
+                # TODO leer el hash_index
+        else: # Inicializar todo
+            self.D = D
+            self.CURRENT_DEPTH = 1
+
+            self.hash_index = [] # key -> [bucket, localdepth]
+            self.hash_index[0] = [0, 1]
+            self.hash_index[1] = [1, 1]
+            self.BUCKETS = 2
+            self.OVERFLOW_BUCKETS = 0
+            self.fb = fb
+
+            with open(filename, "xb") as file:
+                self.write_headers(file)
+        
+        self.BUCKET_SIZE = self.fb * Alumno.SIZE + Bucket.HEADER_SIZE
+        self.MAX_BUCKETS = 2**self.D
+
+    def write_headers(self, file: TextIO):
+        file.seek(0)
+        file.write(struct.pack(self.HEADER_FORMAT, self.D, self.CURRENT_DEPTH, self.BUCKETS, self.OVERFLOW_BUCKETS, self.fb))
+        # TODO escribir el hash_index
 
     def hash(self, num: int) -> int:
         return num % self.MAX_BUCKETS
     
-    def Binary(self, num: int) -> str:
-        return bin(num)
+    def binary(self, num: int) -> str:
+        return bin(num)[2:]
     
-    def create_bucket(self, file: TextIO):
-        file.seek(0, 2)
-        empty = struct.pack("i", -1)
-        data = struct.pack("ii", 0, -1)
-
-        for i in range(self.fb):
-            file.write(empty)
-        file.write(data)
+    def read_bucket(self, file: TextIO, bucket_pos: int) -> Bucket:
+        file.seek(self.BUCKET_SIZE * bucket_pos + self.HEADER_SIZE)
+        return Bucket.unpack(file.read(self.BUCKET_SIZE), self.fb)
     
-    def insert(self, key: int):
-        binary = self.Binary(self.hash(key))
-        while binary not in self.hash_index:
-            binary = binary[1:]
-            
-        bucket = self.hash_index[binary]
-
+    def write_bucket(self, file: TextIO, bucket_pos: int, bucket: Bucket):
+        file.seek(self.BUCKET_SIZE * bucket_pos + self.HEADER_SIZE)
+        file.write(Bucket.pack(bucket))
+    
+    def insert(self, record: Alumno):
         with open(self.filename, "r+b") as file:
-            file.seek(self.BUCKET_SIZE * bucket + self.HEADER_SIZE)
-            data = struct.unpack(self.FORMAT, file.read(self.BUCKET_SIZE))
-            num_reg = data[self.fb]
+            binary = self.binary(self.hash(record.id))
+            binary = binary[-self.CURRENT_DEPTH:]
+            while int(binary, 2) not in self.hash_index:
+                binary = binary[1:]
+                
+            bucket_pos, local_depth = self.hash_index[int(binary, 2)]
+            bucket = self.read_bucket(file, bucket_pos)
 
-            for i in range(0, num_reg):
-                if data[i] == key: # La key ya existe
+            for i in range(0, bucket.size):
+                if bucket.records[i].id == record.id: # La key ya existe
                     return
             
-            if num_reg == self.fb: # Hay que splitear
-                if len(binary) == self.D: # Overflow
-                    next_bucket = data[self.fb + 1]
+            if bucket.is_full(): # Hay que crear un bucket
+                if local_depth == self.D: # Overflow
+                    if bucket.next == -1: # Crear nuevo overflow bucket
+                        new_bucket_pos = self.BUCKETS + self.OVERFLOW_BUCKETS
+                        new_bucket = Bucket(self.fb)
+                        new_bucket.add_record(record)
+                        bucket.next = new_bucket_pos
 
-                    if next_bucket == -1: # Crear nuevo overflow bucket
-                        new_bucket = self.BUCKETS + self.OVERFLOW_BUCKETS
-                        self.create_bucket(file)
-                        file.seek(self.BUCKET_SIZE * (bucket + 1) - 4 + self.HEADER_SIZE) # Escribir puntero al nuevo bucket
-                        file.write(struct.pack("i", new_bucket))
+                        self.write_bucket(file, bucket_pos, bucket)
+                        self.write_bucket(file, new_bucket_pos, new_bucket)
 
-                        file.seek(self.BUCKET_SIZE * new_bucket + self.HEADER_SIZE) # Escribir la key en el nuevo bucket
-                        file.write(struct.pack("i", key))
-
-                        file.seek(self.BUCKET_SIZE * (new_bucket + 1) - 8 + self.HEADER_SIZE) # Escribir tamaño del nuevo bucket en 1
-                        file.write(struct.pack("i", 1))
+                        self.OVERFLOW_BUCKETS += 1
+                        self.write_headers()
                         return
-                    
-                    file.seek(self.BUCKET_SIZE * next_bucket + self.HEADER_SIZE)
-                    data = struct.unpack(self.FORMAT, file.read(self.BUCKET_SIZE))
-                    num_reg = data[self.fb]
-                    bucket = next_bucket
-                    next_bucket = data[self.fb + 1]
 
-                    while True:
-                        for i in range(0, num_reg):
-                            if data[i] == key: # La key ya existe
+                    while True: # Leer el overflow bucket
+                        bucket_pos = bucket.next
+                        bucket = self.read_bucket(file, bucket.next)
+
+                        for i in range(0, bucket.size):
+                            if bucket.records[i].id == record.id: # La key ya existe
                                 return
                         
-                        if next_bucket == -1:
-                            if num_reg == self.fb: # Crear nuevo overflow bucket
-                                new_bucket = self.BUCKETS + self.OVERFLOW_BUCKETS
-                                self.create_bucket(file)
-                                file.seek(self.BUCKET_SIZE * (bucket + 1) - 4 + self.HEADER_SIZE) # Escribir puntero al nuevo bucket
-                                file.write(struct.pack("i", new_bucket))
+                        if bucket.next == -1: # No hay overflow bucket
+                            if bucket.is_full(): # Crear nuevo overflow bucket
+                                new_bucket_pos = self.BUCKETS + self.OVERFLOW_BUCKETS
+                                new_bucket = Bucket(self.fb)
+                                new_bucket.add_record(record)
+                                bucket.next = new_bucket_pos
 
-                                file.seek(self.BUCKET_SIZE * new_bucket + self.HEADER_SIZE) # Escribir la key en el nuevo bucket
-                                file.write(struct.pack("i", key))
+                                self.write_bucket(file, bucket_pos, bucket)
+                                self.write_bucket(file, new_bucket_pos, new_bucket)
 
-                                file.seek(self.BUCKET_SIZE * (new_bucket + 1) - 8 + self.HEADER_SIZE) # Escribir tamaño del nuevo bucket en 1
-                                file.write(struct.pack("i", 1))
-                                return
+                                self.OVERFLOW_BUCKETS += 1
+                                self.write_headers()
                             else: # Se inserta al final
-                                file.seek(self.BUCKET_SIZE * bucket + (num_reg * 4) + self.HEADER_SIZE)
-                                file.write(struct.pack("i", key))
-
-                                num_reg += 1
-                                file.seek(self.BUCKET_SIZE * (bucket + 1) - 8 + self.HEADER_SIZE)
-                                file.write(struct.pack("i", num_reg))
+                                bucket.add_record(record)
+                                self.write_bucket(file, bucket_pos, bucket)
                             return
-                        else:
-                            file.seek(self.BUCKET_SIZE * next_bucket + self.HEADER_SIZE)
-                            data = struct.unpack(self.FORMAT, file.read(self.BUCKET_SIZE))
-                            num_reg = data[self.fb]
-                            bucket = next_bucket
-                            next_bucket = data[self.fb + 1]
-                        
-                else:
-                    # TODO Split
-                    pass
-            else: # Se inserta al final
-                file.seek(self.BUCKET_SIZE * bucket + (num_reg * 4) + self.HEADER_SIZE)
-                file.write(struct.pack("i", key))
+                else: # Split
+                    while True:
+                        # Copiar los valores para insertarlos despues
+                        records = bucket.records
+                        size = bucket.size
 
-                num_reg += 1
-                file.seek(self.BUCKET_SIZE * (bucket + 1) - 8 + self.HEADER_SIZE)
-                file.write(struct.pack("i", num_reg))
+                        # Crear nuevo bucket
+                        new_bucket = Bucket(self.fb)
+                        new_bucket_pos = self.BUCKETS + self.OVERFLOW_BUCKETS
+
+                        # Vaciar bucket original
+                        bucket.size = 0
+
+                        bin1 = "0" + binary[-local_depth:]
+                        bin2 = "1" + binary[-local_depth:]
+
+                        for i in range(size): # Añadir todos los registros a los buckets respectivos
+                            current_record = records[i]
+                            current_bin = self.binary(self.hash(current_record.id))[-(local_depth + 1):]
+                            if current_bin == bin1: # El registro debe ir al bucket original
+                                bucket.add_record(current_record)
+                            else: # El registro debe ir al nuevo bucket
+                                new_bucket.add_record(current_record)
+                        
+                        local_depth += 1
+
+                        # Actualizar el hash index
+                        self.hash_index[int(bin1, 2)] = [bucket_pos, local_depth]
+                        self.hash_index[int(bin2, 2)] = [new_bucket_pos, local_depth]
+
+                        self.BUCKETS += 1
+                        self.CURRENT_DEPTH = max(self.CURRENT_DEPTH, local_depth)
+
+                        # Escribir el nuevo registro
+
+                        # Encontrar el bucket al que debe ir
+                        if binary[-local_depth:] == bin1:
+                            bucket = bucket
+                        else:
+                            bucket = new_bucket
+                        
+                        if bucket.is_full(): # Hay que crear un nuevo bucket
+                            if local_depth == self.D: # Overflow
+                                overflow_bucket_pos = self.BUCKETS + self.OVERFLOW_BUCKETS
+                                overflow_bucket = Bucket(self.fb)
+                                overflow_bucket.add_record(record)
+                                bucket.next = overflow_bucket_pos
+
+                                self.OVERFLOW_BUCKETS += 1
+
+                                self.write_bucket(file, bucket_pos, bucket)
+                                self.write_bucket(file, new_bucket_pos, new_bucket)
+                                self.write_bucket(file, overflow_bucket_pos, overflow_bucket)
+                                self.write_headers()
+
+                                return
+                            else: # Split nuevamente (el bucle while se repite)
+                                self.write_bucket(file, bucket_pos, bucket)
+                                self.write_bucket(file, new_bucket_pos, new_bucket)
+                                self.write_headers()
+                        else: # Se añade al bucket
+                            bucket.add_record(record)
+                            self.write_bucket(file, bucket_pos, bucket)
+                            self.write_bucket(file, new_bucket_pos, new_bucket)
+                            self.write_headers()
+
+                            return
+                        
+            else: # Se inserta al final
+                bucket.add_record(record)
+                self.write_bucket(file, bucket_pos, bucket)
+                return
 
     def search(self, key: int) -> int:
-        pass
-    
+        with open(self.filename, "rb") as file:
+            binary = self.binary(self.hash(key))
+            binary = binary[-self.CURRENT_DEPTH:]
+            while int(binary, 2) not in self.hash_index:
+                binary = binary[1:]
+                
+            bucket_pos, local_depth = self.hash_index[int(binary, 2)]
+
+            while True:
+                bucket = self.read_bucket(file, bucket_pos)
+                for i in range(0, bucket.size):
+                    if bucket.records[i].id == key: # Record encontrado
+                        return bucket.records[i]
+                if bucket.next == -1: # No hay overflow bucket -> el record no existe
+                    return None
+                else: # Revisamos en el overflow bucket
+                    bucket_pos = bucket.next
