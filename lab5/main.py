fe9b8704fe763d2cd17029436ef350d4255c89e9
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-05-04 15:48:19 -0500

    .env para los datos para conectarse a pogre

--- lab5/main.py
+++ lab5/main.py
@@ -34,13 +34,19 @@ CREATE TABLE stopwords (
  
 import psycopg2
 import pandas as pd
+import os
+
+dbname = os.getenv("DBNAME")
+dbuser = os.getenv("DBUSER")
+dbpass = os.getenv("DBPASS")
+dbhost = os.getenv("DBHOST")
 
 def connect_db():
     conn = psycopg2.connect(
-        dbname="<DB>",
-        user="<USER>",
-        password="<PASSWORD>",
-        host="<HOST>"
+        dbname=dbname,
+        user=dbuser,
+        password=dbpass,
+        host=dbhost
     )
     return conn
 
