406d77161d702ae5629fa75511089d3164470430
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-05-08 17:22:43 -0500

    el load.py usa el .env

--- lab5/lab5_2/load.py
+++ lab5/lab5_2/load.py
@@ -1,12 +1,24 @@
 import psycopg2
 import pandas as pd
 
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+dbname = os.getenv("DBNAME")
+dbuser = os.getenv("DBUSER")
+dbpass = os.getenv("DBPASS")
+dbhost = os.getenv("DBHOST")
+
+
 def connect_db():
     conn = psycopg2.connect(
-        dbname="<DB>",
-        user="<USER>",
-        password="<PASSWORD>",
-        host="<HOST>"
+        dbname=dbname,
+        user=dbuser,
+        password=dbpass,
+        host=dbhost
     )
     return conn
 
