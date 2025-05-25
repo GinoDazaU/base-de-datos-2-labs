5bab275287728326b988a65c05a29abe2e686338
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-05-24 20:21:40 -0500

    ya esta el codigo del lab7, falta llenar las tablas y responder las preguntas en el inform

--- lab7/p1.py
+++ lab7/p1.py
@@ -1,14 +1,80 @@
 from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-dry_bean = fetch_ucirepo(id=602) 
-  
-# data (as pandas dataframes) 
-X = dry_bean.data.features 
-y = dry_bean.data.targets 
-  
-# metadata 
-print(dry_bean.metadata) 
-  
-# variable information 
-print(dry_bean.variables) 
+import numpy as np
+import pandas as pd
+import heapq
+
+# fetch dataset
+dry_bean = fetch_ucirepo(id=602)
+data = pd.concat([dry_bean.data.features, dry_bean.data.targets], axis=1)
+
+# Seleccionar las columnas que corresponde a las 16 características
+dataFeatures = data.iloc[:, :16]
+
+# Normalizamos las columnas usando el minimo y el maximo
+dataFeatures = (dataFeatures - dataFeatures.min()) / (dataFeatures.max() - dataFeatures.min())
+dataFeatures.head()
+
+def ED(P, Q):
+    return np.sqrt(np.sum((P - Q) ** 2))
+
+# Seleccionar N pares aleatorias de puntos y calcular la distancia entre ellos
+N = 5000
+data1 = dataFeatures.sample(n=N, random_state=1)
+data2 = dataFeatures.sample(n=N, random_state=2)
+distancias = np.zeros(N)
+for i in range(N):
+    distancias[i] = ED(data1.iloc[i], data2.iloc[i])
+
+# Elaborar un histograma de las distancias
+import matplotlib.pyplot as plt
+plt.hist(distancias, bins = 50)
+plt.show()
+
+# Calculando los percentiles:
+q1 = np.percentile(distancias, 1)
+q5 = np.percentile(distancias, 5)
+q10 = np.percentile(distancias, 10)
+q25 = np.percentile(distancias, 25)
+
+print(f"q1: {q1}, q5: {q5}, q10: {q10}, q25: {q25}")
+
+# El percentil qn deberia devolver aproximadamente un n% de los datos
+# Resultado: q1 = 0.12, q2 = 0.21, q3 = 0.27, q4 = 0.42
+
+def rangeSearch(data, query, radio):
+    result = []
+    for i in range(len(data)):
+        if ED(data.iloc[i], query) <= radio:
+            result.append(i)
+    return result
+
+# Radios seleccionados
+radios = [0.12, 0.21, 0.27, 0.42]
+queries_idx = [15, 2084, 3560]
+
+for qidx in queries_idx:
+    query = dataFeatures.iloc[qidx]
+    target = data.iloc[qidx, -1]
+    for r in radios:
+        result = rangeSearch(dataFeatures, query, r)        
+        PR = 0
+        for i in result:
+            if data.iloc[i, -1] == target:
+                PR += 1
+        PR = PR / len(result)
+        print("Query(" + str(qidx) + ") con radio = " + str(r) + ". Resultados: "+ str(len(result)) + " PR = " + str(PR))
+
+def knnSearch(data, query, k):
+    result = []
+    for i in range(len(data)):
+        dist = ED(data.iloc[i], query)
+        result.append((i, dist))
+    result = heapq.nsmallest(k, result, key=lambda x: x[1])
+    return result
+
+Ks = [2, 4, 8, 16, 32]
+
+for k in Ks:
+    for qidx in queries_idx:
+        print(f"Query({qidx}), k = {k}: ", end='')
+        print(f"{[i[0] for i in knnSearch(dataFeatures, dataFeatures.iloc[qidx], k)]}")
