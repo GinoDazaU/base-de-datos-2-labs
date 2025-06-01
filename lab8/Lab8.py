56ca8b72641d1dfb4417bc108bae2e7839dcbe16
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-05-31 21:48:01 -0500

    ya hice el metodo generate_face_embeddings(), genera los vectores y los mete a una base de datos

--- lab8/Lab8.py
+++ lab8/Lab8.py
@@ -1,2 +1,88 @@
 # En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!En cada pierna o en cada jamón, pero qué buenos los años que tenemos y que aún nos quedan. ¡Felicidades! En este día tan especial, te deseo muchas felicidades y que empieces un año más de vida con el corazón lleno de buenos sentimientos. ¡Feliz cumpleaños, amigo! Que cada paso que des en tu vida te acerque más a la realización de tus sueños. Con todo el cariño, aprecio y respeto que te tengo quiero desearte muchas felicidades en tu onomástico. Que Dios te colme de bendiciones hoy y siempre. Felicitaciones. Mis más sinceras felicitaciones en tu día de cumpleaños, te quiero y te guardo en mi corazón desde el día en que naciste. ¡Que tengas un maravilloso cumpleaños! ¡Espero que tu día especial sea fantástico! ¡Espero que tengas un cumpleaños agradable! Te lo mereces. Estoy feliz de conocerte y espero que tengas un día fantástico. ¡Disfruta este nuevo año!
 # Bro piensa que es cumple de alguien
+import os
+import math
+import face_recognition
+import psycopg2
+from psycopg2.extras import execute_values
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dbname = os.getenv("DBNAME")
+dbuser = os.getenv("DBUSER")
+dbpass = os.getenv("DBPASS")
+dbhost = os.getenv("DBHOST")
+
+def connect_db():
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=dbuser,
+        password=dbpass,
+        host=dbhost
+    )
+    return conn
+
+def distance_to_center(location : list[tuple], center_x : int, center_y : int) -> float:
+    top, right, bottom, left = location
+
+    if left <= center_x <= right:
+        dx = 0
+    elif center_x < left:
+        dx = left - center_x
+    else:
+        dx = center_x - right
+    
+    if top <= center_y <= bottom:
+        dy = 0
+    elif center_y < top:
+        dy = top - center_y
+    else:  # py > bottom
+        dy = center_y - bottom
+    
+    return math.sqrt(dx*dx + dy*dy)
+
+def generate_face_embeddings(dir, N = -1):
+    num = 0
+    res = []
+    print(f"{num} out of {f"? images" if N == -1 else f"{N} images ({round(num/N*100, 2)}%)"}")
+    for root, dirs, files in os.walk(dir):
+        if N != -1 and num >= N:
+            break
+        for file in files:
+            if file.lower().endswith('.jpg'):
+                if N != -1 and num >= N:
+                    break
+                
+                image_path = os.path.join(root, file)
+                image = face_recognition.load_image_file(image_path)
+                faces_locations = face_recognition.face_locations(image)
+                if len(faces_locations) == 0:
+                    continue
+                if len(faces_locations) == 1:
+                    face_location = faces_locations[0]                    
+                if len(faces_locations) > 1:
+                    min_dist = -1
+                    for i in faces_locations:
+                        dist = distance_to_center(i, image.shape[0] // 2, image.shape[1] // 2)
+                        if min_dist == -1 or dist < min_dist:
+                            min_dist = dist
+                            face_location = i
+                face_encoding = face_recognition.face_encodings(image, [face_location])[0]
+                res.append((file, face_encoding.tolist()))
+
+                num += 1
+
+                if(num % 100 == 0):
+                    print(f"{num} out of {f"? images" if N == -1 else f"{N} images ({round(num/N*100, 2)}%)"}")
+    
+    conn = connect_db()
+    cur = conn.cursor()
+    query = "INSERT INTO face_encodings (filename, encoding) VALUES %s"
+    execute_values(cur, query, res)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+generate_face_embeddings('lfw_funneled', 200)
