6b1e1233a97c277bec1aba337fa6171b55b1fb37
GinoDaza <gino.daza@utec.edu.pe>
2025-06-01 20:04:43 -0500

    lab8 p1

--- lab8/p1.sql
+++ lab8/p1.sql
@@ -6,7 +6,7 @@ CREATE EXTENSION IF NOT EXISTS cube;
 -- 2 dimensiones
 CREATE TABLE vectors2 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors2(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 2)))
+SELECT id, cube(ARRAY[random()*1000, random()*1000])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors2 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors2 ON vectors2 USING gist(vector_gist);
@@ -14,15 +14,20 @@ CREATE INDEX idx_vectors2 ON vectors2 USING gist(vector_gist);
 -- 4 dimensiones
 CREATE TABLE vectors4 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors4(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 4)))
-FROM generate_series(1, 1000000) id;
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000, random()*1000
+])
+FROM generate_series(1, 100000) id;
 UPDATE vectors4 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors4 ON vectors4 USING gist(vector_gist);
 
 -- 6 dimensiones
 CREATE TABLE vectors6 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors6(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 6)))
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000
+])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors6 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors6 ON vectors6 USING gist(vector_gist);
@@ -30,7 +35,10 @@ CREATE INDEX idx_vectors6 ON vectors6 USING gist(vector_gist);
 -- 8 dimensiones
 CREATE TABLE vectors8 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors8(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 8)))
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000
+])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors8 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors8 ON vectors8 USING gist(vector_gist);
@@ -38,7 +46,12 @@ CREATE INDEX idx_vectors8 ON vectors8 USING gist(vector_gist);
 -- 16 dimensiones
 CREATE TABLE vectors16 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors16(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 16)))
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000
+])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors16 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors16 ON vectors16 USING gist(vector_gist);
@@ -46,7 +59,16 @@ CREATE INDEX idx_vectors16 ON vectors16 USING gist(vector_gist);
 -- 32 dimensiones
 CREATE TABLE vectors32 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors32(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 32)))
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000
+])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors32 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors32 ON vectors32 USING gist(vector_gist);
@@ -54,10 +76,28 @@ CREATE INDEX idx_vectors32 ON vectors32 USING gist(vector_gist);
 -- 64 dimensiones
 CREATE TABLE vectors64 (id serial, vector_lineal cube, vector_gist cube);
 INSERT INTO vectors64(id, vector_lineal)
-SELECT id, cube(array(select random()*1000 FROM generate_series(1, 64)))
+SELECT id, cube(ARRAY[
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000,
+  random()*1000, random()*1000, random()*1000, random()*1000
+])
 FROM generate_series(1, 1000000) id;
 UPDATE vectors64 SET vector_gist = vector_lineal;
 CREATE INDEX idx_vectors64 ON vectors64 USING gist(vector_gist);
+
 
 -- D2 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -65,7 +105,7 @@ SELECT id, vector_lineal,
        cube_distance(vector_lineal, '(100, 200)') AS D
   FROM vectors2
  ORDER BY vector_lineal <-> '(100, 200)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D2 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -73,7 +113,7 @@ SELECT id, vector_gist,
        cube_distance(vector_gist, '(100, 200)') AS D
   FROM vectors2
  ORDER BY vector_gist <-> '(100, 200)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D4 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -81,7 +121,7 @@ SELECT id, vector_lineal,
        cube_distance(vector_lineal, '(100, 200, 300, 400)') AS D
   FROM vectors4
  ORDER BY vector_lineal <-> '(100, 200, 300, 400)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D4 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -89,7 +129,7 @@ SELECT id, vector_gist,
        cube_distance(vector_gist, '(100, 200, 300, 400)') AS D
   FROM vectors4
  ORDER BY vector_gist <-> '(100, 200, 300, 400)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D6 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -97,7 +137,7 @@ SELECT id, vector_lineal,
        cube_distance(vector_lineal, '(100, 200, 300, 400, 500, 600)') AS D
   FROM vectors6
  ORDER BY vector_lineal <-> '(100, 200, 300, 400, 500, 600)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D6 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -105,7 +145,7 @@ SELECT id, vector_gist,
        cube_distance(vector_gist, '(100, 200, 300, 400, 500, 600)') AS D
   FROM vectors6
  ORDER BY vector_gist <-> '(100, 200, 300, 400, 500, 600)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D8 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -113,7 +153,7 @@ SELECT id, vector_lineal,
        cube_distance(vector_lineal, '(100,200,300,400,500,600,700,800)') AS D
   FROM vectors8
  ORDER BY vector_lineal <-> '(100,200,300,400,500,600,700,800)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D8 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -121,7 +161,7 @@ SELECT id, vector_gist,
        cube_distance(vector_gist, '(100,200,300,400,500,600,700,800)') AS D
   FROM vectors8
  ORDER BY vector_gist <-> '(100,200,300,400,500,600,700,800)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D16 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -129,7 +169,7 @@ SELECT id, vector_lineal,
        cube_distance(vector_lineal, '(100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115)') AS D
   FROM vectors16
  ORDER BY vector_lineal <-> '(100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D16 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -137,7 +177,7 @@ SELECT id, vector_gist,
        cube_distance(vector_gist, '(100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115)') AS D
   FROM vectors16
  ORDER BY vector_gist <-> '(100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115)'
- LIMIT 5;
+ LIMIT 3;
 
 -- D32 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -153,7 +193,7 @@ SELECT id, vector_lineal,
          11,12,13,14,15,16,17,18,19,20,
          21,22,23,24,25,26,27,28,29,30,31,32
        )'
- LIMIT 5;
+ LIMIT 3;
 
 -- D32 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -169,7 +209,7 @@ SELECT id, vector_gist,
          11,12,13,14,15,16,17,18,19,20,
          21,22,23,24,25,26,27,28,29,30,31,32
        )'
- LIMIT 5;
+ LIMIT 3;
 
 -- D64 KNN using lineal scan
 EXPLAIN ANALYZE
@@ -193,7 +233,7 @@ SELECT id, vector_lineal,
          51,52,53,54,55,56,57,58,59,60,
          61,62,63,64
        )'
- LIMIT 5;
+ LIMIT 3;
 
 -- D64 KNN using the GiST index
 EXPLAIN ANALYZE
@@ -217,4 +257,9 @@ SELECT id, vector_gist,
          51,52,53,54,55,56,57,58,59,60,
          61,62,63,64
        )'
- LIMIT 5;
+ LIMIT 3;
+
+-- Usar antes de cada consulta
+DISCARD PLANS;
+DISCARD ALL;
+VACUUM FULL vectors4;
